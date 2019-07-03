96bd400c95ab3352b44526667d9b9ee9cc6a3d22
yuiant <yuiant@qq.com>
2019-07-03 18:15:17 +0800

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+setup(
+    name='pyalgomuse',
+    description='Package for Pratice in ai music',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    test_suite="tests",
+    zip_safe=False)
